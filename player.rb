8f1204950ab1f48c1a83e20cf8d20d443d0586d2
Jeremy Heaton <jeremy.a.heaton@gmail.com>
2018-04-16 22:44:00 -0400

    level 6 solution

--- player.rb
+++ player.rb
@@ -1,21 +1,38 @@
 class Player
   MIN_HEALTH = 15
+  @rescued = false
 
   def play_turn(warrior)
-    puts @health
-    if warrior.feel.captive?
+    if warrior.feel(:backward).captive?
+      warrior.rescue!(:backward)
+      @rescued = true
+    elsif !warrior.feel(:backward).wall? && !@rescued
+      warrior.walk!(:backward)
+    elsif warrior.feel.captive?
       warrior.rescue!
     elsif warrior.feel.enemy?
       warrior.attack!
-    elsif warrior.health < MIN_HEALTH && unharmed?(warrior)
-      warrior.rest!
+    elsif heal?(warrior)
+      if !attacked?(warrior)
+        warrior.rest!
+      else
+        warrior.walk!(:backward)
+      end
     else
       warrior.walk!
     end
     @health = warrior.health
   end
 
-  def unharmed?(warrior)
-    warrior.health >= @health
+  def heal?(warrior)
+    healing?(warrior) || warrior.health < 14
+  end
+
+  def healing?(warrior)
+    warrior.health < 20 && warrior.health > @health
+  end
+
+  def attacked?(warrior)
+     @health > warrior.health
   end
 end
