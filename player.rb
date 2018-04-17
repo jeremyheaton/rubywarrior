2aaf9b54bf3e289b1178b1a5c1193b3ef76fbeea
Jeremy Heaton <jeremy.a.heaton@gmail.com>
2018-04-16 22:00:05 -0400

    level 3 solution

--- player.rb
+++ player.rb
@@ -1,7 +1,12 @@
 class Player
+  MIN_HEALTH = 8
+
   def play_turn(warrior)
+    puts @minimum
     if warrior.feel.enemy?
       warrior.attack!
+    elsif warrior.health < MIN_HEALTH
+      warrior.rest!
     else
       warrior.walk!
     end
